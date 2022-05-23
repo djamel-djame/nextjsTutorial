0484f9cec93a10b42962a02da72c3b9b3b471873
djamelabbou <djamelabbou@outlook.com>
2022-05-24 00:02:04 +0100

    <Image />

--- pages/first-post.js
+++ pages/first-post.js
@@ -1,9 +1,15 @@
 import React from "react";
 import Link from "next/link";
+import Image from "next/image"
 export default function FirstPost() {
   return (
     <>
       <h1>hi</h1>
+      <Image
+        src="../public/vercel.svg"
+        height={144}
+        width={144}
+      />
       <Link href="/">
         <a style={{textDecoration: 'none', fontSize: "10em", color: "black"}}>Now go back</a>
       </Link>
